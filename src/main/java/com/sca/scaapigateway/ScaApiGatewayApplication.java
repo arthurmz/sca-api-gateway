72b9dff0a147dc8810aea00cb48063a93b2e2ebf
Arthur <arthur.jmacedo@gmail.com>
2020-06-08 01:51:59 -0300

    Rotas para o sca-cadastro-ativos

---
+++ src/main/java/com/sca/scaapigateway/ScaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sca.scaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sca/scaapigateway/ScaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sca.scaapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
